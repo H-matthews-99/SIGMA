b069d8c533b91c7efcd0c138056cd08076d4ce36
H-matthews-99 <iqmalhidayat456@gmail.com>
2026-06-30 01:09:01 +0700

    detail kecil

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://sb_publishable_a72mysIayb6rGM5I_LcYJw_GBoJDkqv';
+const supabaseUrl = 'https://yucxomntawxgzfsvowbr.supabase.co/rest/v1/';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1Y3hvbW50YXd4Z3pmc3Zvd2JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3NDEyNTksImV4cCI6MjA5ODMxNzI1OX0.yMCF_Y9K3MshixdEFW5Yf1iPbwOl-6mQCxlUAUulYi8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
